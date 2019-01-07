18b4d964fd61cdde5420c5073d9c00ec8db131fd
zhangle <319126583@qq.com>
2019-01-07 16:49:32 +0800

    获取地理位置和手机号，详情页面分享

--- src/pages/detail/index.vue
+++ src/pages/detail/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="container">
     <BookInfo :info="bookInfo"></BookInfo>
+    <div class="switch">
+      <div class="location">
+        地理位置：
+        <switch class="checkbox" @change="getLoc" :checked="location" color="#DD4E41"></switch>
+        <span>{{location}}</span>
+      </div>
+      <div class="phone">
+        手机型号：
+        <switch class="checkbox" @change="getPhone" :checked="phone" color="#DD4E41"></switch>
+        <span>{{phone}}</span>
+      </div>
+    </div>
+    <button class="btn" @click="addComment" v-if="showAdd">评论</button>
   </div>
 </template>
 
@@ -14,7 +27,9 @@
     data () {
       return {
         bookid: '',
-        bookInfo: {}
+        bookInfo: {},
+        location: '',
+        phone: ''
       }
     },
     methods: {
@@ -25,11 +40,63 @@
         wx.setNavigationBarTitle({
           title: this.bookinfo.title
         })
+      },
+      getPhone (e) {
+        if (e.target.value) {
+          const phoneInfo = wx.getSystemInfoSync()
+          this.phone = phoneInfo.model
+        } else {
+          this.phone = ''
+        }
+      },
+      getLoc (e) {
+        const ak = 't4QRSgpPVuta6KdYkrtZp2u3jhnxnarI'
+        const url = 'http://api.map.baidu.com/geocoder/v2/'
+        const that = this
+        if (e.target.value) {
+          wx.getLocation({
+            success (geo) {
+              console.log(geo)
+              wx.request({
+                url,
+                data: {
+                  ak,
+                  location: `${geo.latitude},${geo.longitude}`,
+                  output: 'json'
+                },
+                success (res) {
+                  if (res.data.status === 0) {
+                    console.log(res.data)
+                    that.location = res.data.result.addressComponent.city
+                  } else {
+                    that.location = '未知'
+                  }
+                }
+              })
+            }
+          })
+        } else {
+          this.location = ''
+        }
+      },
+      onShareAppMessage: function (res) {
+        if (res.from === 'button') {
+          console.log(res.target)
+        }
+        return {
+          title: '自定义转发标题'
+        }
       }
     },
     mounted () {
       this.bookid = this.$root.$mp.query.id
       this.getDetail()
+    },
+    onShareAppMessage (res) {
+      console.log(res.target)
+      return {
+        title: this.bookInfo.title
+      }
     }
   }
 </script>
